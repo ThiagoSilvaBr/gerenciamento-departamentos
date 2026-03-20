031ec4c8f8768ea2c64fb267b4b95280d812529c
Thiago Henrique <164083408+ThiagoSilvaBr@users.noreply.github.com>
2026-03-20 17:20:37 -0300

    feat: Add DepartamentoRepository

---
+++ src/main/java/com/bn/departments/repositories/DepartamentoRepository.java
@@ -0,0 +1,7 @@
+package com.bn.departments.repositories;
+
+import com.bn.departments.models.DepartamentoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartamentoRepository extends JpaRepository<DepartamentoModel, Long> {
+}
